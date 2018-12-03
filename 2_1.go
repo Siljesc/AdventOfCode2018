adc96da3d56d885c5be0bb92e98e816134d93295
SunriseM <sunrisem.js@gmail.com>
2018-12-03 12:23:04 -0400

    Add exercise 2_1

---
+++ 2_1.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("2.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	twoWords := 0
+	threeWords := 0
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		letterList := strings.Split(word, "")
+		letterDict := map[string]int{}
+
+		counted2 := false
+		counted3 := false
+
+		for _, letter := range letterList {
+
+			_, ok := letterDict[letter]
+
+			if !ok {
+				letterDict[letter] = 0
+			}
+
+			letterDict[letter]++
+		}
+
+		for _, frequency := range letterDict {
+			if frequency == 2 && !counted2 {
+				twoWords++
+				counted2 = true
+			} else if frequency == 3 && !counted3 {
+				threeWords++
+				counted3 = true
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("File scanning error", err)
+	}
+
+	fmt.Println("Checksum: ", twoWords*threeWords)
+
+}
