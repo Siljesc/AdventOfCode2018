75240b9db34bda8685e9a216e5c49607ef5b3427
SunriseM <sunrisem.js@gmail.com>
2018-12-03 09:13:47 -0400

    Exercise 1_1

---
+++ 1_1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("1_1.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	freq := 0
+
+	for scanner.Scan() {
+		lineFreq, lineErr := strconv.Atoi(scanner.Text())
+
+		if lineErr != nil {
+			fmt.Println("String casting to int error", err)
+			return
+		}
+
+		freq += lineFreq
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("File scanning error", err)
+	}
+
+	fmt.Println("Frequency: ", freq)
+
+}
